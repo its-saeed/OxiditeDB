d82e36eddada7c986e46ecba0644f0fdb67a9e57
Saeed Dadkhah <sa.dadkhah@gmail.com>
2025-06-04 16:51:14 +0330

    Support basic commands like insert and select.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,54 @@
-use rustyline::error::ReadlineError;
-use rustyline::{DefaultEditor, Result};
+use anyhow::{anyhow, Result};
+use rustyline::DefaultEditor;
+
+trait Statement {
+    fn execute(&self) -> Result<()>;
+}
+
+trait MetaCommand {
+    fn execute(&self) -> Result<()>;
+}
+
+struct ExitMetaCommand;
+impl MetaCommand for ExitMetaCommand {
+    fn execute(&self) -> Result<()> {
+        std::process::exit(0)
+    }
+}
+
+struct InsertStatement;
+struct SelectStatement;
+
+impl Statement for InsertStatement {
+    fn execute(&self) -> Result<()> {
+        println!("Executing insert statement");
+        Ok(())
+    }
+}
+
+impl Statement for SelectStatement {
+    fn execute(&self) -> Result<()> {
+        println!("Executing select statement");
+        Ok(())
+    }
+}
+
+fn parse_meta_command(cmd: &str) -> Result<Box<dyn MetaCommand>> {
+    match cmd {
+        ".exit" => Ok(Box::new(ExitMetaCommand)),
+        cmd => Err(anyhow!("Unrecognized command: {cmd}")),
+    }
+}
+
+fn parse_statement(cmd: &str) -> Result<Box<dyn Statement>> {
+    if cmd.starts_with("select") {
+        Ok(Box::new(SelectStatement))
+    } else if cmd.starts_with("insert") {
+        Ok(Box::new(InsertStatement))
+    } else {
+        Err(anyhow!("Unrecognized command: {cmd}"))
+    }
+}
 
 fn main() -> Result<()> {
     let mut rl = DefaultEditor::new()?;
@@ -8,18 +57,34 @@ fn main() -> Result<()> {
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str())?;
-                match line.as_str() {
-                    ".exit" => return Ok(()),
-                    cmd => eprintln!("Unrecognized command {cmd}"),
+                // Non-SQL statements like .exit are called “meta-commands”.
+                if line.starts_with('.') {
+                    match parse_meta_command(&line) {
+                        Ok(cmd) => {
+                            if let Err(e) = cmd.execute() {
+                                eprintln!("{e}");
+                                continue;
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("{e}");
+                            continue;
+                        }
+                    }
+                } else {
+                    match parse_statement(&line) {
+                        Ok(statement) => {
+                            if let Err(e) = statement.execute() {
+                                eprintln!("{e}");
+                                continue;
+                            }
+                        }
+                        Err(e) => {
+                            eprintln!("{e}");
+                            continue;
+                        }
+                    }
                 }
-            }
-            Err(ReadlineError::Interrupted) => {
-                println!("CTRL-C");
-                break;
-            }
-            Err(ReadlineError::Eof) => {
-                println!("CTRL-D");
-                break;
             }
             Err(err) => {
                 println!("Error: {:?}", err);
