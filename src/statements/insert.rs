87c5fb448b8d34052f4ffd3d1246bec44de51fd0
Saeed Dadkhah <sa.dadkhah@gmail.com>
2025-06-05 13:11:16 +0330

    Use pest to parse statements like insert or select.

--- src/statements/insert.rs
+++ src/statements/insert.rs
@@ -1,4 +1,16 @@
-pub struct Insert;
+const USERNAME_COLUMN_SIZE: usize = 32;
+const EMAIL_COLUMN_SIZE: usize = 255;
+
+pub struct Row {
+    pub id: u32,
+    pub username: String,
+    pub email: String,
+}
+
+pub struct Insert {
+    pub row: Row,
+}
+
 impl super::Statement for Insert {
     fn execute(&self) -> anyhow::Result<()> {
         println!("Executing insert statement");
--- src/statements/mod.rs
+++ src/statements/mod.rs
@@ -2,20 +2,45 @@ mod insert;
 mod select;
 
 pub use insert::Insert;
+use pest::Parser;
+use pest_derive::Parser;
 pub use select::Select;
 
 use anyhow::{anyhow, Result};
+
+use crate::statements::insert::Row;
 
 pub trait Statement {
     fn execute(&self) -> Result<()>;
 }
 
+#[derive(Parser)]
+#[grammar = "statements/statements.pest"]
+struct StatementParser;
+
 pub fn parse_statement(cmd: &str) -> Result<Box<dyn Statement>> {
-    if cmd.starts_with("select") {
-        Ok(Box::new(Select))
-    } else if cmd.starts_with("insert") {
-        Ok(Box::new(Insert))
-    } else {
-        Err(anyhow!("Unrecognized command: {cmd}"))
+    let statement = StatementParser::parse(Rule::statement, cmd.trim())
+        .unwrap()
+        .next()
+        .unwrap();
+
+    let inner_statement = statement.into_inner().next().unwrap();
+
+    match inner_statement.as_rule() {
+        Rule::INSERT => {
+            let mut inner = inner_statement.into_inner();
+            let id = inner.next().unwrap().as_str().parse::<u32>().unwrap();
+            let username = inner.next().unwrap().as_str();
+            let email = inner.next().unwrap().as_str();
+            Ok(Box::new(Insert {
+                row: Row {
+                    id,
+                    username: username.to_string(),
+                    email: email.to_string(),
+                },
+            }))
+        }
+        Rule::SELECT => Ok(Box::new(Select)),
+        _ => Err(anyhow!("Unrecognized command: {cmd}")),
     }
 }
